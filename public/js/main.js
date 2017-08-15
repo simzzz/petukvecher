4db69aa9ab758961ec4250b3e028099465b5c4a6
simzzz <swagb3ast@hotmail.com>
2017-08-15 23:47:39 +0300

    Added facebook and twitter authentication

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,20 @@
-$(document).ready(function() {
+$(document).ready(() => {
+  window.fbAsyncInit = function () {
+      FB.init({
+          appId: '1892812197704452',
+          xfbml: true,
+          version: 'v2.10'
+        });
+      FB.AppEvents.logPageView();
+    };
 
-  // Place JavaScript code here...
-
+  (function (d, s, id) {
+      let js,
+          fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = '//connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 });
